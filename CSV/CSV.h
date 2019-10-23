2b3e050518c82e6c25a69e1735bf97f16320f7dc
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2019-10-23 10:35:26 +0200

    Add framework target

---
+++ CSV/CSV.h
@@ -0,0 +1,19 @@
+//
+//  CSV.h
+//  CSV
+//
+//  Created by Matthias Hochgatterer on 23.10.19.
+//  Copyright © 2019 Matthias Hochgatterer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CSV.
+FOUNDATION_EXPORT double CSVVersionNumber;
+
+//! Project version string for CSV.
+FOUNDATION_EXPORT const unsigned char CSVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSV/PublicHeader.h>
+
+
